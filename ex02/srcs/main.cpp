79b4f3f7fece59a49bb6ba4f73459597edd861e6
erpascua <erpascua@student.42.fr>
2026-02-19 19:17:45 +0100

    (ex02): Try/Catch method added to main

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 11:03:36 by erpascua          #+#    #+#             */
-/*   Updated: 2026/02/19 18:57:58 by erpascua         ###   ########.fr       */
+/*   Updated: 2026/02/19 19:16:44 by erpascua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,34 +21,60 @@ int main()
 {
 	srand(time(0));
 	// 
+
 	std::cout << "=== VALID: Shrubbery Creation Form ===\n";
-	Bureaucrat 				florentMotte;
-	ShrubberyCreationForm	fmotte("fmottefmotte");
-	
-	fmotte.execute(florentMotte);
-	std::cout << "Grade to sign: " << fmotte.getGradeToSign() << "\nGrade to exec: " << fmotte.getGradeToExecute() << "\n";
+	try
+	{
+		Bureaucrat 				florentMotte;
+		ShrubberyCreationForm	fmotte("fmottefmotte");
+		fmotte.execute(florentMotte);
+		std::cout << "Grade to sign: " << fmotte.getGradeToSign() << "\nGrade to exec: " << fmotte.getGradeToExecute() << "\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << '\n';
+	}
 	
 	// 
 	std::cout << "\n=== VALID: Robotomy Request Form ===\n";
-	Bureaucrat				guillaumePollastri;
-	RobotomyRequestForm		gpollast("La Pollast");
-
-	gpollast.execute(guillaumePollastri);
-	std::cout << "Grade to sign: " << gpollast.getGradeToSign() << "\nGrade to exec: " << gpollast.getGradeToExecute() << "\n";
+	try
+	{
+		Bureaucrat				guillaumePollastri;
+		RobotomyRequestForm		gpollast("La Pollast");
+		gpollast.execute(guillaumePollastri);
+		std::cout << "Grade to sign: " << gpollast.getGradeToSign() << "\nGrade to exec: " << gpollast.getGradeToExecute() << "\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
 	// 
-	// std::cout << "\n=== VALID: Presidential Pardon Form ===\n";
-	// Bureaucrat				tomLorette;
-	// PresidentialPardonForm	tlorette("La Lorette");
-
-	// tlorette.execute(tomLorette);
-	// std::cout << "Grade to sign: " << tlorette.getGradeToSign() << "\nGrade to exec: " << tlorette.getGradeToExecute() << "\n";
-
+	std::cout << "\n=== VALID: Presidential Pardon Form ===\n";
+	try
+	{
+		Bureaucrat				tomLorette;
+		PresidentialPardonForm	tlorette("La Lorette");
+		tlorette.execute(tomLorette);
+		std::cout << "Grade to sign: " << tlorette.getGradeToSign() << "\nGrade to exec: " << tlorette.getGradeToExecute() << "\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
 	// 
 	std::cout << "\n=== INVALID: Presidential Pardon Form ===\n";
-	Bureaucrat				benSuger;
-	PresidentialPardonForm	bSuger("La Lorette");
-
-	bSuger.execute(benSuger);
-	std::cout << "Grade to sign: " << bSuger.getGradeToSign() << "\nGrade to exec: " << bSuger.getGradeToExecute() << "\n";
+	try
+	{
+		Bureaucrat				benSuger;
+		PresidentialPardonForm	bSuger("La Lorette");
+	
+		bSuger.execute(benSuger);
+		std::cout << "Grade to sign: " << bSuger.getGradeToSign() << "\nGrade to exec: " << bSuger.getGradeToExecute() << "\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	} 
 }
