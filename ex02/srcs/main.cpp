676400690fb704a923929d19f0a95bd56dd3b267
erpascua <erpascua@student.42.fr>
2026-02-19 15:13:39 +0100

    (ex02): Firsts tests

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,17 +6,21 @@
 /*   By: erpascua <erpascua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 11:03:36 by erpascua          #+#    #+#             */
-/*   Updated: 2026/02/19 11:06:13 by erpascua         ###   ########.fr       */
+/*   Updated: 2026/02/19 14:55:35 by erpascua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/AForm.hpp"
 #include "../includes/Bureaucrat.hpp"
-#include "../includes/PresidentialPardonForm.hpp"
-#include "../includes/RobotomyRequestForm.hpp"
 #include "../includes/ShrubberyCreationForm.hpp"
+// #include "../includes/PresidentialPardonForm.hpp"
+// #include "../includes/RobotomyRequestForm.hpp"
 
 int main()
 {
+	
+	Bureaucrat 				florentMotte;
+	ShrubberyCreationForm	fmotte("fmottefmotte");
 
+	fmotte.execute(florentMotte);
 }
